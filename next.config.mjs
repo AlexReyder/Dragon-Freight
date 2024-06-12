0aab99b92510f9b52645558291fec45b7f616b6f
Alex Reyder <razrab@bk.ru>
2024-06-12 20:15:30 +0300

    2

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'https://simter-mc.ru/',
-				port: '',
-				pathname: '**',
-			},
-		],
-	},
-	env: {
-		domainUrl: process.env.DOMAIN,
-		emailUser: process.env.EMAIL_LOGIN,
-		emailPass: process.env.EMAIL_PASS,
-	},
+	// images: {
+	// 	remotePatterns: [
+	// 		{
+	// 			protocol: 'https',
+	// 			hostname: 'https://simter-mc.ru/',
+	// 			port: '',
+	// 			pathname: '**',
+	// 		},
+	// 	],
+	// },
+	// env: {
+	// 	domainUrl: process.env.DOMAIN,
+	// 	emailUser: process.env.EMAIL_LOGIN,
+	// 	emailPass: process.env.EMAIL_PASS,
+	// },
 	// cacheBust: true,
 	typescript: {
 		ignoreBuildErrors: true,
