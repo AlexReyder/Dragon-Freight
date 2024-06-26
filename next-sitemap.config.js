0f52a64ae94962ad6343e7cc5c5d48b13c2401ce
Alex Reyder <razrab@bk.ru>
2024-06-26 14:02:15 +0300

    sitemapo!

--- next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-	siteUrl: process.env.DOMAIN || 'https://dragonfreight.ru/',
-	generateRobotsTxt: true, // (optional)
-	// ...other options
-}
