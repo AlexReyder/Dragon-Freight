4343e887ea48d38a790a5e529809bf2c6a37c11b
Alex Reyder <razrab@bk.ru>
2024-07-05 13:19:12 +0300

    yandex metrica

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 
 const inter = Inter({
@@ -38,15 +39,6 @@ export const metadata: Metadata = {
 		locale: 'ru_RU',
 		type: 'website',
 	},
-	icons: {
-		icon: '/icon.png',
-		shortcut: '/shortcut-icon.png',
-		apple: '/apple-icon.png',
-		other: {
-			rel: 'apple-touch-icon-precomposed',
-			url: '/apple-touch-icon-precomposed.png',
-		},
-	},
 	alternates: {
 		canonical: './',
 	},
@@ -63,7 +55,27 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='ru'>
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				{children}
+				<Script id='metrika-counter' strategy='afterInteractive'>
+					{`
+	<!-- Yandex.Metrika counter -->
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(97753412, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+<!-- /Yandex.Metrika counter -->
+	`}
+				</Script>
+			</body>
 		</html>
 	)
 }
